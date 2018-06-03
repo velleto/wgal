2e583b06fdb867d60af2b76c708749acba16e0ac
velleto <rr@velleto.com>
2018-06-03 17:52:47 +0200

    Modified utils.find() to check for directory existence, instead of returning an empty list.
    Signed-off-by: velleto <rr@velleto.com>

--- wgal/utils.py
+++ wgal/utils.py
@@ -1,9 +1,18 @@
 # Copyright (C) 2018 Ryan Rueger
 
+import sys
 import os
 
 
 def find(path):
+    """Check whether directory exists, and recursively list files it contains."""
+
+    try:
+        if not os.path.isdir(path):
+            raise Exception("No such directory: {}".format(path))
+    except Exception as error:
+        print("wgal ERROR: find: " + " ".join(error.args))
+        sys.exit(1)
 
     all_files = []
     for parent, child, files in os.walk(path):
