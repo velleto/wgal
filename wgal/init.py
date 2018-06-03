e933116f0e1be8e4ba4416a6bceacf8b6908ba99
velleto <rr@velleto.com>
2018-06-03 23:13:26 +0200

    Checks whether the user defined destination path contains an existing instance of a web album. If not, all necessary directories are created and CSS/JSS files copied.
    Signed-off-by: velleto <rr@velleto.com>

--- wgal/init.py
+++ wgal/init.py
@@ -4,6 +4,8 @@
 import os
 import sys
 import argparse
+import pathlib
+import shutil
 
 import wgal.utils
 
@@ -24,6 +26,7 @@ class WebGallery(object):
         args = self.__parse_options()
 
         self.__check_paths(args)
+        self.__todo(args)
 
     def __parse_options(self):
         """Parse command line options."""
@@ -35,7 +38,8 @@ class WebGallery(object):
 
         parser.add_argument("web",
                            metavar="WEB",
-                           help="Specify destination directory for the website.")
+                           help="Specify destination \
+                                directory for the website.")
 
         args = parser.parse_args()
 
@@ -43,12 +47,18 @@ class WebGallery(object):
 
     def __check_paths(self, paths):
         """Check whether configuration paths are read-/writable."""
-
         try:
             for file in wgal.utils.find(paths.source):
                 if not os.access(file, os.R_OK):
                     raise Exception(
                         "Source path not readable: {}".format(paths.source))
+
+            if not os.path.isdir(paths.web):
+                print("wgal WARNING: Creating website directory: {}"
+                        .format(paths.web))
+
+            pathlib.Path(paths.web).mkdir(exist_ok=True)
+
             for file in wgal.utils.find(paths.web):
                 if not os.access(file, os.R_OK | os.W_OK):
                     raise Exception(
@@ -57,8 +67,79 @@ class WebGallery(object):
             print("wgal ERROR: " + " ".join(error.args))
             sys.exit(1)
 
-    def __todo(self):
-        """Check differences between current source and active album."""
+    def __todo(self, args):
+        """Check differences between current source and active album.
+
+        Firstly, check whether args.web points to an existing instance
+        of a web gallery. If not create a new one.
+        Then go through each source image, and note those who's
+        derivatives have not been created.
+
+        The tree of a web gallery instance looks like this
+        .
+        ├── m
+        │   ├── f
+        │   ├── p
+        │   └── t
+        ├── r
+        │   ├── css
+        │   │   ├── progressive-image.css
+        │   │   ├── progressive-image.min.css
+        │   │   ├── single.css
+        │   │   └── tiled.css
+        │   └── js
+        │       ├── progressive-image.js
+        │       └── progressive-image.min.js
+        └── s
+        """
+        # Where the templates are kept.
+        root = os.path.dirname(wgal.__file__)
+        originals = os.path.join(root, "website")
+        # First, define where we expect all the resource/media files
+        # to be relative to the root of any generic web album instance.
+        # Then check if the instance defined by paths.web contains these
+        # files.
+        rel_resources_root = "r"
+        # Resource directories for CSS and JS.
+        rel_resources_types = ["css", "js"]
+        rel_resources = [os.path.join(rel_resources_root, typ)
+                    for typ in rel_resources_types]
+        rel_media_root = "m"
+        # Media directories for Full, Preview and Thumbnail quality images.
+        rel_media_types = ["f", "p", "t"]
+        rel_media = [os.path.join(rel_media_root, typ)
+                    for typ in rel_media_types]
+        # Directory containing single photo html files.
+        rel_single = "s"
+
+        css = [os.path.join(rel_resources[0], css_file)
+                for css_file in
+                ["progressive-image.css",
+                "progressive-image.min.css",
+                "single.css",
+                "tiled.css"]]
+        js = [os.path.join(rel_resources[1], js_file)
+                for js_file in
+                ["progressive-image.js",
+                "progressive-image.min.js"]]
+
+        # Paths of directories in the current instance as
+        # specified in args.web.
+        web_resources = [os.path.join(args.web, typ) for typ in rel_resources]
+        web_media = [os.path.join(args.web, typ) for typ in rel_media]
+        web_single = os.path.join(args.web, rel_single)
+
+        for dir in [*web_resources, *web_media, web_single]:
+            if not os.path.isdir(dir):
+                print("wgal WARNING: Creating directory: {}".format(dir))
+            pathlib.Path(dir).mkdir(exist_ok=True, parents=True)
+
+        for file in [*css, *js]:
+            web_file = os.path.join(args.web, file)
+            original_file = os.path.join(originals, file)
+            if not os.path.isfile(web_file):
+                print("wgal WARNING: Creating file: {}".format(file))
+                shutil.copy2(original_file, web_file)
 
     def __process_images(self):
         """Create derivatives of the source files and create thumbnails."""
