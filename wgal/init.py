d9584e790e198761735feda2b7bf46a4a5f7dc02
velleto <rr@velleto.com>
2018-06-03 13:30:22 +0200

    Implemented option parsing, to read SOURCE and DEST paths from command line.
    Signed-off-by: velleto <rr@velleto.com>

--- wgal/init.py
+++ wgal/init.py
@@ -1,23 +1,41 @@
 # wgal initialisation code.
 # Copyright (C) 2018 Ryan Rueger and Contributors.
 
+import argparse
+
 
 class WebGallery(object):
     """The main class that encapsulates the high level use of WebGallery.
 
     To run wgal, call:
 
-        import wgal
+    >>> import wgal
 
-        wg = WebGallery()
-        wg.run()
+    >>> wg = WebGallery()
+    >>> wg.run()
     """
 
     def run(self):
         """Invoke everything."""
+        args = self.__parse_options()
 
     def __parse_options(self):
         """Parse command line options."""
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument('source',
+                            metavar='SOURCE',
+                            nargs=1,
+                            help="Specify source directory of images.")
+
+        parser.add_argument('web',
+                            metavar='WEB',
+                            nargs=1,
+                            help="Specify destination directory for the website.")
+
+        args = parser.parse_args()
+
+        return args
 
     def __check_paths(self):
         """Check whether configuration paths are read-/writable."""
